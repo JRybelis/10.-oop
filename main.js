f4c9ff358bbe853858707b53a3895e491c4319fc
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-03-12 09:32:42 +0200

    method chaining

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 class User {
-    constructor(username, email){
+    constructor(username, email, score){
         //set up properties of the object
         this.username = username;
         this.email = email;
+        this.score = 0;
     }
     login(){
         console.log(`${this.username} just logged in.`);
+        return this;
     }
     logout(){
         console.log(`${this.username} logged out.`);
+        return this;
+    }
+    incrScore(){
+        this.score ++;
+        console.log(`${this.username} has a score of ${this.score}`);
+        return this;
     }
 }
 
@@ -19,7 +27,7 @@ const userTwo = new User('Luigi', 'luigi@thenetninja.co.uk');
 // 2. binds the value of 'this' to the new empty object inside the class being used.
 // 3. calls the constructor function to 'build' the object. 'this' keyword refers to that specific class object created. 
 
-userOne.login();
-userOne.logout();
-userTwo.login();
-userTwo.logout();
+
+//chaining methods:
+userOne.login().incrScore().incrScore().logout();
+userTwo.login().incrScore().incrScore().logout();
